547aef4fc58ab17a1cecf7a16e375d4c721c29fe
Sanika <yadgiresanika291@gmail.com>
2026-09-16 16:57:47 +0530

    completed issue book task

--- src/com/bookmate/storage/DataStore.java
+++ src/com/bookmate/storage/DataStore.java
@@ -5,18 +5,24 @@ import java.util.List;
 
 import com.bookmate.model.Author;
 import com.bookmate.model.Book;
+import com.bookmate.model.BookCopy;
 import com.bookmate.model.Category;
 import com.bookmate.model.IssueRecord;
+import com.bookmate.model.Member;
 
 public class DataStore {
 	
-	private static List<Book> books = new ArrayList<>();
+	private static final List<Book> books = new ArrayList<>();
 	
-	private static List<Author> authors = new ArrayList<>();
+	private static final List<Author> authors = new ArrayList<>();
 	
-	private static List<Category> categories = new ArrayList<>();
+	private static final List<Category> categories = new ArrayList<>();
 	
-	private static List<IssueRecord> issues = new ArrayList<>();
+	private static final List<IssueRecord> issues = new ArrayList<>();
+	
+	private static final List<Member> members=new ArrayList<> ();
+	
+	private static final List<BookCopy> copies = new ArrayList<>();
 	
 	public static List<Author> getAuthors(){
 		return authors;
@@ -32,6 +38,14 @@ public class DataStore {
 	
 	public static List<IssueRecord> getIssues(){
 		return issues;
+	}
+	
+	public static List<Member> getMembers(){
+		return members;
+	}
+	
+	public static List<BookCopy> getcopies(){
+		return copies ;
 	}
 
 }
