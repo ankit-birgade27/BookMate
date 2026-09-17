07cd5d46606ed34661f5c5e3cfa7053d541af6c0
Sanika <yadgiresanika291@gmail.com>
2026-09-17 17:29:14 +0530

    getmemberbyId implemented

--- src/com/bookmate/dao/MemberDao.java
+++ src/com/bookmate/dao/MemberDao.java
@@ -10,6 +10,44 @@ public class MemberDao {
 	public List<Member> findAll(){
 		return new ArrayList<>(DataStore.getMembers());
 	}
+	
+	public void saveMember(Member member) {
+		DataStore.getMembers().add(member);
+	}
+	
+	public Member findById(String memberId) {
+		List<Member> members=DataStore.getMembers();
+			
+		for(Member m:members) {
+			if(memberId.equals(m.getMemberId())) {
+				return m;
+			}
+		}
+		return null;
+		
+	}
+	
+	public Member findByEmail(String email) {
+		List<Member> members=DataStore.getMembers();
+		
+		for(Member m:members) {
+			if(email.equals(m.getContactInfo().getEmail())) {
+				return m;
+			}
+		}
+		return null;
+	}
+	
+	public Member findByMobile(String mobile) {
+		List<Member> members=DataStore.getMembers();
+		
+		for(Member m:members) {
+			if(mobile.equals(m.getContactInfo().getMobileNumber())) {
+				return m;
+			}
+		}
+		return null;
+	}
 }
 
 
