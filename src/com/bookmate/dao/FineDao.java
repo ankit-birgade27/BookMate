eb6ee1245fb37c64a50eca23284cde042f8cdcb9
Sanika <yadgiresanika291@gmail.com>
2026-09-19 16:40:39 +0530

    Fine calculation implemented

--- src/com/bookmate/dao/FineDao.java
+++ src/com/bookmate/dao/FineDao.java
@@ -1,9 +1,41 @@
 package com.bookmate.dao;
 
+import java.util.List;
+
 import com.bookmate.model.Fine;
+import com.bookmate.model.IssueRecord;
 import com.bookmate.storage.DataStore;
 
 public class FineDao {
+	
+	private Issuedao issueDao = new Issuedao();
+
+    public IssueRecord findIssueById(String issueId) {
+        return issueDao.findById(issueId);
+    }
+
+    public Fine findByIssueId(String issueId) {
+    	
+        for (Fine fine : DataStore.getFines()) {
+            if (issueId.equals(fine.getIssueRecord().getIssueId())) {
+                return fine;
+            }
+        }
+
+        return null;
+    }
+
+    public void saveFine(Fine fine) {
+    	
+    		List<Fine> fines = DataStore.getFines();
+        Fine existingFine = findByIssueId(fine.getIssueRecord().getIssueId());
+
+        if (existingFine != null) {
+            fines.remove(existingFine);
+        }
+
+        fines.add(fine);
+    }
 	
 	public Fine findById(String fineId) {
 		for(Fine fine : DataStore.getFines()) {
---
+++ src/com/bookmate/exception/FineCalculationException.java
@@ -0,0 +1,8 @@
+package com.bookmate.exception;
+
+public class FineCalculationException extends RuntimeException {
+	public FineCalculationException(String msg) {
+		super(msg);
+	}
+
+}
